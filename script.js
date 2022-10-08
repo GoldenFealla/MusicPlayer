d61fb7f40eddeae7bc8b2cea7c5a71264a852077
Khanh Nguyen <khanh.nlq14917@sinhvien.hoasen.edu.vn>
2022-10-09 00:40:21 +0700

    make it responsive

--- script.js
+++ script.js
@@ -2,6 +2,37 @@ const display = document.getElementById('display');
 const play_svg = "./images/play.svg";
 const pause_svg = "./images/pause.svg";
 
+let resizeRatio = 1;
+//window resize
+const root = document.querySelector(':root').style;
+
+const ratio = 0.8;
+
+let width;
+let height;
+
+window.onload = showDisplay;
+window.onresize = showDisplay;
+
+function showDisplay() {
+    width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) * ratio;
+    height = width * 0.5625;
+
+    root.setProperty('--res-width', width + 'px');
+    root.setProperty('--res-height', height + 'px');
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < width) {
+        root.setProperty('--res-width', window.innerWidth  + 'px');
+        root.setProperty('--res-height', window.innerWidth / width + 'px');
+    }
+    else {
+        root.setProperty('--res-width', width + 'px');
+        root.setProperty('--res-height', height + 'px');
+    }
+    recalculateBarWidth();
+});
 
 //Handle Import File
 const au = document.getElementById('au');
@@ -9,19 +40,31 @@ const bg = document.getElementById('bg');
 
 au.addEventListener('change', () => {
     let tempFileURL = URL.createObjectURL(au.files[0]);
+
+    //change audio src
     audio.src = tempFileURL;
 });
 
 bg.addEventListener('change', () => {
     let tempFileURL = URL.createObjectURL(bg.files[0]);
-    console.log(tempFileURL);
+
+    //change background after load
+    background.style.backgroundImage = `url('${tempFileURL}')`;
+    background.style.backgroundSize = "cover";
 });
+//background
+const background = document.getElementById('background')
 
 //audio
 const audio = new Audio();
 
-audio.addEventListener('timeupdate', () => {
-    slide.value = audio.currentTime;
+audio.addEventListener('play', () => {
+    playButton.src = pause_svg;
+    slideChange();
+});
+
+audio.addEventListener('pause', () => {
+    playButton.src = play_svg;
 });
 
 audio.addEventListener('ended', () => {
@@ -52,16 +95,20 @@ playButton.addEventListener('click', () => {
 
     if (audio.paused) {
         audio.play();
-        playButton.src = pause_svg;
     } else {
         audio.pause();
-        playButton.src = play_svg;
     }
 });
 
-volume.addEventListener('change', (e) => {
+volume.addEventListener('input', (e) => {
     audio.volume = e.target.value / 100;
 });
+
+slideChange = () => {
+    slide.value = Math.round(audio.currentTime * 100) / 100;
+    if(audio.paused) return;
+    requestAnimationFrame(slideChange);
+}
 
 slide.addEventListener('input', (e) => {
     audio.currentTime = e.target.value;
@@ -70,20 +117,15 @@ slide.addEventListener('input', (e) => {
 
     if (audio.paused) {
         audio.play();
-        playButton.src = pause_svg;
     }
 });
 
 
 //audio reaction
+
+//event to resize canvas
 const canvas = document.getElementById('audio-reaction');
-
-canvas.width = 400;
-canvas.height = 300;
-
 const ctx = canvas.getContext('2d');
-
-ctx.translate(canvas.width / 2, canvas.height / 2);
 
 const audioReaction = {
     isConfigured: false,
@@ -104,6 +146,10 @@ const config = {
     fftSize: 2 ** 14,
 }
 
+const recalculateBarWidth = () => {
+    audioReactionData.barWidth = (width * 0.5) / (audioReaction.analyser.frequencyBinCount / 32);
+}
+
 function audioReactionStart ({ isConfigured, audioCtx, analyser, audioSource} = audioReaction, { bufferLength, dataArray, barWidth } = audioReactionData) {
     if(!isConfigured) {
         audioCtx = new (window.AudioContext)();
@@ -112,29 +158,44 @@ function audioReactionStart ({ isConfigured, audioCtx, analyser, audioSource} = 
         audioSource.connect(analyser);
         analyser.connect(audioCtx.destination);
         isConfigured = true;
+
+        Object.assign(audioReaction, { isConfigured, audioCtx, analyser, audioSource });
+        Object.assign(analyser, config);
     }
 
-    Object.assign(analyser, config);
-    
     bufferLength = analyser.frequencyBinCount;
     dataArray = new Uint8Array(analyser.frequencyBinCount);
-    barWidth = canvas.width / (analyser.frequencyBinCount / 32);
+    barWidth = (width * 0.5) / (analyser.frequencyBinCount / 32);
 
-    Object.assign(audioReaction, { isConfigured, audioCtx, analyser, audioSource });
     Object.assign(audioReactionData, { bufferLength, dataArray, barWidth });
-
-    animate(audioReactionData, audioReaction);
+    animate(audioReactionData, audioReaction, canvas, ctx);
 }
 
-function animate({ bufferLength, dataArray, barWidth } = audioReactionData, { analyser } = audioReaction) {
+const animate = function ({ bufferLength, dataArray, barWidth } = audioReactionData, { analyser } = audioReaction) {
     x = 0;
+    ctx.translate(canvas.width / 2, canvas.height / 2);
     ctx.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
     analyser.getByteFrequencyData(dataArray);
+
     for (let i = 0; i < bufferLength / 32; i++) {
         let barHeight = dataArray[i] / 2;
         ctx.fillStyle = "white";
         ctx.fillRect(-canvas.width / 2 + x, - barHeight - 1, barWidth, barHeight + 1);
         x += barWidth;
     }
+
+    let buffer = document.createElement('canvas');
+    let bufferCtx = buffer.getContext('2d');
+    buffer.width = canvas.width;
+    buffer.height = canvas.height;
+    buffer.style.display = "none";
+
+    bufferCtx.drawImage(canvas, 0, 0);
+
+    canvas.width = width * 0.5;
+    canvas.height = width * 0.25;
+
+    ctx.drawImage(buffer, 0, 0, canvas.width , canvas.height);
+
     requestAnimationFrame(animate.bind(null, audioReactionData, audioReaction));
 }
